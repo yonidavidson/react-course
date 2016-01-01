82d27bb42002ad4c96348f7d13e476eb39c499fe
Boris Dinkevich <boris@dinkevich.com>
2016-01-01 00:00:00 +0000

    Allow adding recipes

--- app/app.js
+++ app/app.js
@@ -19,14 +19,7 @@ class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      recipes: recipes
-    };
-
-    setTimeout(
-      () => this.setState({recipes: recipes.concat('Shakshuka')}),
-      1000
-    );
+    this.state = { recipes };
   }
 
   render() {
@@ -34,8 +27,21 @@ class App extends React.Component {
       <div>
         <h1>Recipes:</h1>
         <Recipes recipes={ this.state.recipes }/>
+
+        <form onSubmit={ this.onSubmit.bind(this) }>
+          <input ref="title" type="text"/>
+          <button>Add</button>
+        </form>
       </div>
     );
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.setState({ recipes: this.state.recipes.concat(this.refs.title.value) });
+
+    this.refs.title.value = '';
   }
 }
 
