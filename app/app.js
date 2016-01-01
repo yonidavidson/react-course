ed6d36800ec21e44b7fc1d220737d82ad4d2b58f
Boris Dinkevich <boris@dinkevich.com>
2016-01-01 00:00:00 +0000

    Use ES2015 features

--- app/app.js
+++ app/app.js
@@ -3,14 +3,12 @@ import 'assets/stylesheets/main.scss';
 
 import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div>
-      <h1>I am a component!</h1>
-      <h2>I am a sub title</h2>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>I am a component!</h1>
+    <h2>I am a sub title</h2>
+  </div>
+);
 
 render(
   React.createElement(App),
