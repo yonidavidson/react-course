1d94f273fff5eb52af0e149693166243893e2166
Boris Dinkevich <boris@dinkevich.com>
2016-01-01 00:00:00 +0000

    Add combine reducers

--- app/reducers/root.js
+++ app/reducers/root.js
@@ -1,26 +1,6 @@
-import { ADD_RECIPE, TOGGLE_FAVORITE } from 'consts/action-types';
+import { combineReducers } from 'redux';
+import recipesReducer from './recipes';
 
-const reducer = (state, action) => {
-
-  switch (action.type) {
-    case ADD_RECIPE:
-      const newRecipe = Object.assign(action.payload, { favorite: false });
-
-      return Object.assign({}, state, {
-        recipes: state.recipes.concat(newRecipe)
-      });
-
-    case TOGGLE_FAVORITE:
-      const recipes = state.recipes.map(recipe =>
-        recipe.title !== action.payload
-          ? recipe
-          : Object.assign({}, recipe, { favorite: !recipe.favorite })
-      );
-
-      return Object.assign({}, state, { recipes });
-  }
-
-  return state;
-};
-
-export default reducer;
+export default combineReducers({
+  recipes: recipesReducer
+});
