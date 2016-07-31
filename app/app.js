6d01a73bfa1ad17805a4f7c86f1d40f675815a3a
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-07-31 12:46:39 +0300

    first special component

--- app/app.js
+++ app/app.js
@@ -3,9 +3,15 @@ import 'assets/stylesheets/main.scss';
 
 import { render } from 'react-dom';
 
-function App() {
-  return React.createElement('h1', null, 'I am a component!');
-}
+const Math = ({x,y}) => (<h2>{x+y}</h2>) // Destructering from props
+
+const App = () => (
+ <div>
+  <h1>I am a component</h1> // Main component is between tags
+  <Math x={1} y={2}></Math> //Propes are part of first tag 
+  <Math x={2} y={3}></Math> 
+  </div>
+);
 
 render(
   React.createElement(App),
