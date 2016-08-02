8b38b943b6f44f0ebf292f2b7fac8b318e4f8aac
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-08-02 14:27:17 +0300

    almost

--- app/components/recipe-details.js
+++ app/components/recipe-details.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 const RecipeDetails = (props) => (
   <div>
     <h1>{props.params.id}</h1>
-    <h2>{props.description}</h2>
+    <h2>{props.description} 'meta-data'</h2>
     <Link to="/">"Home"</Link>
   </div>
 );
 
 const mapStateToProps = (state, ownProps) => ({
-  description : JSON.stringify(ownProps.routeParams.id)
-});
+    description : JSON.stringify(state.recipes.find(recipe => recipe.title === ownProps.params.title)) 
+  });
 
 export default connect(mapStateToProps, null)(RecipeDetails);
