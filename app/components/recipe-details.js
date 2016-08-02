9772c206a3ad2e636b0cb4519df9734eff0c0411
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-08-02 14:20:02 +0300

    get ownProps from distpach

--- app/components/recipe-details.js
+++ app/components/recipe-details.js
@@ -1,10 +1,16 @@
 import { Link, browserHistory } from 'react-router';
+import { connect } from 'react-redux';
 
 const RecipeDetails = (props) => (
   <div>
     <h1>{props.params.id}</h1>
-    <Link to="/">HOME</Link>
+    <h2>{props.description}</h2>
+    <Link to="/">"Home"</Link>
   </div>
 );
 
-export default RecipeDetails
+const mapStateToProps = (state, ownProps) => ({
+  description : JSON.stringify(ownProps.routeParams.id)
+});
+
+export default connect(mapStateToProps, null)(RecipeDetails);
